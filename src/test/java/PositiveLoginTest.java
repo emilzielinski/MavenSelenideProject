0f543256b023dbacc8972c3b1baf408eed858373
EM <emil0900@gmail.com>
2023-07-17 17:13:04 +0200

    Changes to name of the PositiveLoginTest.java name

--- src/test/java/PositiveLoginTest.java
+++ src/test/java/PositiveLoginTest.java
@@ -3,16 +3,16 @@ import org.testng.annotations.Test;
 import page.objects.LandingPage;
 import page.objects.ProductsPage;
 
-public class PositiveLoginTests extends TestBase {
+public class PositiveLoginTest extends TestBase {
 
     @Test
-    public void successfulLoginWithGoodCredentials(){
+    public void successfulLoginWithGoodCredentialsTest(){
 
         LandingPage landingPage = new LandingPage();
         ProductsPage productsPage = new ProductsPage();
 
         landingPage.loginField.sendKeys("standard_user");
-        landingPage.passwordField.sendKeys("secret_sauc");
+        landingPage.passwordField.sendKeys("secret_sauce");
         landingPage.loginButton.click();
 
         Assert.assertEquals(productsPage.appLogo.getText(), "Swag Labs");
